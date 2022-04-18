60a65beef25356cb3753ba8216d1c9b1a68960a3
Practicum AmitBG217 <amit217@yandex.com>
2022-04-18 11:17:46 +0300

    feat+style: Create different objects

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,16 +6,23 @@ export default function Card(props) {
   return (
     <div className={CardCSS.card}>
       <img className={CardCSS.card__image} src={props.imageUrl} alt="place" />
-      <div className={CardCSS.cardbrief}>
-        <img
-          className={CardCSS.cardbrief__logo}
-          src={loclogo}
-          alt="location logo"
-        />
-        <h2 className={CardCSS.cardbrief__location}>{props.location}</h2>
-        <a className={CardCSS.cardbrief__link} href={props.googleMapsUrl}>
-          View on Google Maps
-        </a>
+      <div className={CardCSS.cardContent}>
+        <div className={CardCSS.cardbrief}>
+          <img
+            className={CardCSS.cardbrief__logo}
+            src={loclogo}
+            alt="location logo"
+          />
+          <h2 className={CardCSS.cardbrief__location}>{props.location}</h2>
+          <a className={CardCSS.cardbrief__link} href={props.googleMapsUrl}>
+            View on Google Maps
+          </a>
+        </div>
+        <h1 className={CardCSS.cardContent__title}>{props.title}</h1>
+
+        <time className={CardCSS.cardContent__date}>{props.startDate} -</time>
+        <time className={CardCSS.cardContent__date}> {props.endDate}</time>
+        <p className={CardCSS.cardContent__description}>{props.description}</p>
       </div>
     </div>
   );
